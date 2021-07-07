1ad79f64b2070b53b21fd59b22d3bdd947a2f3b6
gitdev-bash <63194733+gitdev-bash@users.noreply.github.com>
2021-07-07 20:44:26 +0000

    Fix: Issues-in-alpha_fix-1 (#6)
    * FIX:issues-in-alpha_fix-1
    
    Fix for:
        https://github.com/gitdev-bash/webArchiver/projects/2#card-59124678
        https://github.com/gitdev-bash/webArchiver/projects/2#card-59124654
    
    * FIX:issues-in-alpha_fix-2
    
    Fix for:
        https://github.com/gitdev-bash/webArchiver/projects/2#card-59128817
    
    * FIX:issues-in-alpha_fix-3
    
    Fix for:
        https://github.com/gitdev-bash/webArchiver/projects/2#card-59124588
        https://github.com/gitdev-bash/webArchiver/projects/2#card-59124626
    
    * FIX:issues-in-alpha_fix-4
    
    Fix for:
        https://github.com/gitdev-bash/webArchiver/projects/2#card-59124716
        https://github.com/gitdev-bash/webArchiver/projects/2#card-51933053
    
    * Small fixes with Flake8
    
    * Update README.md
    
    * Typo Fixes to Readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # webArchiver
 webArchiver is a project in alpha with the main purpose of Archiving Websites but can be used to archived anything.   
 It saves space by dealing with duplicates, keeps all files in a central location and keeps logs.   
-##### Other new functions like cgi web interfaces, automatic archiving of websites and other terminal functions are comming!   
-Being in alpha you might see python error messages from python when dealing with already existing files or folders.   
+#### Other new functions like CGI web interfaces, automatic archiving of websites and other terminal functions are coming!   
+Being in alpha you might see python error messages in edge cases: different devices, broken names, wrong CSV files.   
 If you encounter a bug or misspelling please report it at https://github.com/gitdev-bash/webArchiver/issues.   
 All contributions are welcome.  
--- webArchiver.py
+++ webArchiver.py
@@ -6,79 +6,11 @@ import datetime
 import argparse
 import shutil
 import configparser
-# set vars
-archivedir1 = os.path.expanduser("~") + "/archiver/files/"
-archivedir2 = os.path.expanduser("~") + "/archiver/archive/"
-csv1 = archivedir1 + "csv1"
-csv2 = archivedir1 + "csv2"
-datetimeformat = "%y.%m.%d-%H:%M"
-datetimes = datetime.datetime.now().strftime(datetimeformat)
-hashblocksize = 65536
-hashertype = "sha256"
-configfile = os.path.expanduser("~") + "/archiver/archiver.conf"
-# readtags
-piparser = argparse.ArgumentParser()
-piparser.add_argument("-fi", "--folderin", help="Input folder")
-piparser.add_argument("-fs", "--foldersave",
-                      help="Archive save directory location")
-piparser.add_argument("-fa", "--folderarchive",
-                      help="Archive archiving directory location")
-piparser.add_argument("-fl1", "--filelist1", help="Archive filelist1 location")
-piparser.add_argument("-fl2", "--filelist2", help="Archive filelist2 location")
-piparser.add_argument("-dtf", "--datetimeformat", help="Set datetime format")
-piparser.add_argument("-hb", "--hashblocksize", help="Set hash blocksize")
-piparser.add_argument("-ht", "--hashtype",
-                      help="""
-        Set hash type to one of:'blake2s', 'sha384',
-        'sha512', 'sha3_256', 'sha256', 'md5', 'sha3_512',
-        'sha3_224', 'shake_128', 'shake_256', 'sha1',
-        'blake2b', 'sha224', 'sha3_384'""")
-piparser.add_argument("-c", "--configfile",
-                      help="Set configfile in init format (in [DEFAULT])")
-piargs = piparser.parse_args()
-if piargs.configfile is not None:
-    configfile = piargs.configfile
-# read config file
-config = configparser.ConfigParser()
-if os.path.isfile(configfile):
-    config.read(configfile)
-    if "DEFAULT" in config:
-        if "folderarchive" in config["DEFAULT"]:
-            archivedir2 = config["DEFAULT"]["folderarchive"]
-        if "filelist1" in config["DEFAULT"]:
-            csv1 = config["DEFAULT"]["filelist1"]
-        if "filelist2" in config["DEFAULT"]:
-            csv2 = config["DEFAULT"]["filelist2"]
-        if "datetimeformat" in config["DEFAULT"]:
-            datetimeformat = config["DEFAULT"]["datetimeformat"]
-        if "hashblocksize" in config["DEFAULT"]:
-            hashblocksize = int(config["DEFAULT"]["hashblocksize"])
-        if "hashtype" in config["DEFAULT"]:
-            hashertype = config["DEFAULT"]["hashtype"]
-        if "foldersave" in config["DEFAULT"]:
-            archivedir1 = config["DEFAULT"]["foldersave"]
-##
-if piargs.folderin is None:
-    print("Give input folder")
-    exit(1)
-if piargs.foldersave is not None:
-    archivedir1 = piargs.foldersave
-if piargs.folderarchive is not None:
-    archivedir2 = piargs.folderarchive
-if piargs.filelist1 is not None:
-    csv1 = piargs.filelist1
-if piargs.filelist2 is not None:
-    csv2 = piargs.filelist2
-if piargs.datetimeformat is not None:
-    datetimeformat = piargs.datetimeformat
-if piargs.hashblocksize is not None:
-    hashblocksize = int(piargs.hashblocksize)
-if piargs.hashtype is not None:
-    hashertype = piargs.hashtype
 
 
 # define functions
 def file_len(fname):
+    """Returns the number of lines in a file."""
     if not os.path.isfile(fname):
         return(0)
     with open(fname) as f:
@@ -89,12 +21,14 @@ def file_len(fname):
 
 
 def forcedir(file_path):
+    """Forces a directories existence."""
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
 
 
 def hasher(filein, hname, blocksize):
+    """Generates the hash of the file given."""
     htype = getattr(hashlib, hname, print)()
     if htype is None:
         print("wrong hash type")
@@ -108,54 +42,136 @@ def hasher(filein, hname, blocksize):
     return(htype.hexdigest())
 
 
-def csver(filein, operation, data):
-    if operation == "find":
-        if not os.path.isfile(filein):
-            return("False")
-        with open(filein, 'rt') as fi:
-            reader = csv.reader(fi, delimiter=',')
-            for row in reader:
-                hashout = row[1]  # location of hash
-                if hashout == data:
-                    return(row[0])
-            return("False")
-    elif operation == "append":
-        if not os.path.isfile(filein):
-            pass
-        with open(filein, 'a') as fd:
-            writer = csv.writer(fd)
-            writer.writerow(data)
+def csv_find(filein, data):
+    """Finds and returns the row number of the element given, in a CSV file."""
+    if not os.path.isfile(filein):
+        return(-1)
+    with open(filein, 'rt') as fi:
+        reader = csv.reader(fi, delimiter=',')
+        for row in reader:
+            hashout = row[1]  # location of hash
+            if hashout == data:
+                return(row[0])
+        return(-1)
 
 
-def wfile(filedef, filerel):
+def csv_append(filein, data):
+    """Appends the given data to a CSV file."""
+    if not os.path.isfile(filein):
+        pass
+    with open(filein, 'a') as fd:
+        writer = csv.writer(fd)
+        writer.writerow(data)
+
+
+def file_worker(filedef, filerel):
+    """Moves the file into another directory and then
+    , replaces the file given with a symbolic link."""
     hashin = hasher(filedef, hashertype, hashblocksize)
-    csvid = csver(csv1, "find", hashin)
-    if csvid == "False":
+    csvid = csv_find(csv1, hashin)
+    if csvid == -1:
         csvid = file_len(csv1)+1
-        csver(csv1, "append", [csvid, hashin])
+        csv_append(csv1, [csvid, hashin])
         shutil.move(filedef, archivedir1 + "/" + str(csvid))
     else:
         os.remove(filedef)
-    csver(csv2, "append", [datetimes, csvid, filerel])
+    csv_append(csv2, [datetimes, csvid, filerel])
     os.symlink(archivedir1 + "/" + str(csvid), filedef)
 
 
-def wfolder(directory):
+def folder_worker(directory):
+    """Works down the directory tree of a given folder recursively."""
     for ff in os.listdir(directory):
         ffdef = directory + "/" + ff
         if os.path.isfile(ffdef):
-            wfile(ffdef, ff)
+            file_worker(ffdef, ff)
         else:
-            wfolder(ffdef)
+            folder_worker(ffdef)
 
 
-# main program
-forcedir(archivedir1)
-forcedir(archivedir2)
-infolder = os.path.abspath(piargs.folderin)
-if not os.path.exists(infolder):
-    print("Folder not found!")
-    exit(1)
-wfolder(infolder)
-shutil.move(infolder, archivedir2 + "/")
-exit(0)
+if __name__ == "__main__":
+    # set vars
+    archivedir1 = os.path.expanduser("~") + "/archiver/files/"
+    archivedir2 = os.path.expanduser("~") + "/archiver/archive/"
+    csv1 = archivedir1 + "csv1"
+    csv2 = archivedir1 + "csv2"
+    datetimeformat = "%y.%m.%d-%H:%M"
+    datetimes = datetime.datetime.now().strftime(datetimeformat)
+    hashblocksize = 65536
+    hashertype = "sha256"
+    configfile = os.path.expanduser("~") + "/archiver/archiver.conf"
+    # readtags
+    piparser = argparse.ArgumentParser()
+    piparser.add_argument("-fi", "--folderin", help="Input folder")
+    piparser.add_argument("-fs", "--foldersave",
+                          help="Archive save directory location")
+    piparser.add_argument("-fa", "--folderarchive",
+                          help="Archive archiving directory location")
+    piparser.add_argument("-fl1", "--filelist1",
+                          help="Archive filelist1 location")
+    piparser.add_argument("-fl2", "--filelist2",
+                          help="Archive filelist2 location")
+    piparser.add_argument("-dtf", "--datetimeformat",
+                          help="Set datetime format")
+    piparser.add_argument("-hb", "--hashblocksize", help="Set hash blocksize")
+    piparser.add_argument("-ht", "--hashtype",
+                          help="""
+            Set hash type to one of:'blake2s', 'sha384',
+            'sha512', 'sha3_256', 'sha256', 'md5', 'sha3_512',
+            'sha3_224', 'shake_128', 'shake_256', 'sha1',
+            'blake2b', 'sha224', 'sha3_384'""")
+    piparser.add_argument("-c", "--configfile",
+                          help="Set configfile in init format (in [DEFAULT])")
+    piargs = piparser.parse_args()
+    if piargs.configfile is not None:
+        configfile = piargs.configfile
+    # read config file
+    config = configparser.ConfigParser()
+    if os.path.isfile(configfile):
+        config.read(configfile)
+        if "DEFAULT" in config:
+            if "folderarchive" in config["DEFAULT"]:
+                archivedir2 = config["DEFAULT"]["folderarchive"]
+            if "filelist1" in config["DEFAULT"]:
+                csv1 = config["DEFAULT"]["filelist1"]
+            if "filelist2" in config["DEFAULT"]:
+                csv2 = config["DEFAULT"]["filelist2"]
+            if "datetimeformat" in config["DEFAULT"]:
+                datetimeformat = config["DEFAULT"]["datetimeformat"]
+            if "hashblocksize" in config["DEFAULT"]:
+                hashblocksize = int(config["DEFAULT"]["hashblocksize"])
+            if "hashtype" in config["DEFAULT"]:
+                hashertype = config["DEFAULT"]["hashtype"]
+            if "foldersave" in config["DEFAULT"]:
+                archivedir1 = config["DEFAULT"]["foldersave"]
+    ##
+    if piargs.folderin is None:
+        print("Give input folder")
+        exit(1)
+    if piargs.foldersave is not None:
+        archivedir1 = piargs.foldersave
+    if piargs.folderarchive is not None:
+        archivedir2 = piargs.folderarchive
+    if piargs.filelist1 is not None:
+        csv1 = piargs.filelist1
+    if piargs.filelist2 is not None:
+        csv2 = piargs.filelist2
+    if piargs.datetimeformat is not None:
+        datetimeformat = piargs.datetimeformat
+    if piargs.hashblocksize is not None:
+        hashblocksize = int(piargs.hashblocksize)
+    if piargs.hashtype is not None:
+        hashertype = piargs.hashtype
+#    main program
+    forcedir(archivedir1)
+    forcedir(archivedir2)
+    infolder = os.path.abspath(piargs.folderin)
+    if not os.path.exists(infolder):
+        print("Folder not found!")
+        exit(1)
+    if os.path.isdir(archivedir2 + "/" + os.path.basename(infolder)):
+        print("Folder exists in destination!")
+        exit(1)
+    folder_worker(infolder)
+    shutil.move(infolder, archivedir2 + "/" + os.path.basename(infolder))
+    exit(0)
